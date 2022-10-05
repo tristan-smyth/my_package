7349bc16492fa15bff0894f0a6e4d2a19df31ac9
tristan-smyth <tristan.smyth@hotmail.co.uk>
2022-10-05 19:34:49 +0100

    first commit

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -5,6 +5,6 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='demo_nodes_py',
-            executable='talker'
+            executable='listener'
         )
     ])
